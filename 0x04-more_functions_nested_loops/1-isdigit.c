60421c4f55886e336ca19f01f1f155517eb30d8d
Ntsika2 <Ntsikaqumba@yahoo.com>
2022-10-20 18:39:52 -0700

    is digital

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,26 +1,17 @@
 #include "main.h"
 /**
- * _isdigit - checks if character digit
+ * _isdigit -checks if character digit
  * @c: Variable text
  * Return: Always 0
  */
 int _isdigit(int c)
 {
+	char 'c';
 
-	char c;
-
-	switch (c)
+	if (c ≥ '0' && c  ≤ '9')
 	{
-
-	case '0':
-	       _putchar("%c: %d\n", c);
-		break;
-	case 'a':
-		_putchar("%c: %d\n", c);
-		break;
-	default:
-		_putchar("\nIncorrect input");
-		break;
+		return (1);
 	}
-	return (0);
+
+		return (0);
 }
