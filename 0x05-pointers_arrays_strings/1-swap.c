eb16bd7e7e1a65b26c0f80434a3e76149965410a
Ntsika2 <Ntsikaqumba@yahoo.com>
2022-10-24 04:48:48 -0700

    void swap two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *swap_int - sawp two int
+ *a: int one
+ *@b: int two
+ *
+ *Return: 0 Success
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
