af055b976104972c3fe0e0483cd1500e9c3d5008
Ntsika2 <Ntsikaqumba@yahoo.com>
2022-10-25 17:32:19 -0700

    removed spaces

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * _puts() - prints the string
  *
- * Return: Always 0.
+ * Return: string
  */
 void _puts(char *str)
 {
-	
+
 	while (*str != '\0')
 	_putchar(*str++);
 	_putchar('\n');
